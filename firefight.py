bae5083bf7103fe49f139e900d576f1267031fa7
Rupert Eardley <rupert.eardley@gmail.com>
2022-12-20 21:20:39 +0000

    Complete model points calculation; needs more testing

--- firefight.py
+++ firefight.py
@@ -159,43 +159,47 @@ class Weapon:
 class Model:
     def __init__(
         self,
+        name,
         quality,
         defense,
         toughness,
-        arsenal=None,
         cover=None,
         courage=False,
         command=False,
         deflect=False,
         droid=False,
         emplacement=False,
-        expendable=False,
+        expendable=0,
         fast=False,
         fear=False,
         fly=False,
-        heal=False,
-        hero_villain=False,
-        hunter=False,
+        heal=0,
+        hero=False,
+        villain=False,
+        hunter=None,
         immobile=False,
-        jedi_sith=False,
-        jump=False,
-        impact=None,
+        jedi=False,
+        sith=False,
+        jump=0,
+        impact=0,
         impervious=False,
         protector=False,
         relay=False,
         relentless=False,
-        repair=None,
+        repair=0,
         scout=False,
-        shield=None,
+        shield=0,
         slow=False,
-        spotter=None,
-        take_cover=None,
-        vehicle=None,
+        spotter=0,
+        take_cover=0,
+        unique=False,
+        vehicle=False,
     ) -> None:
+        self.name = name
         self.quality = quality
         self.defense = defense
         self.toughness = toughness
-        self.arsenal = arsenal
+        # ignoring arsenal for now; may not need it to exist in the rules?
         self.cover = cover
         self.courage = courage
         self.command = command
@@ -207,10 +211,12 @@ class Model:
         self.fear = fear
         self.fly = fly
         self.heal = heal
-        self.hero_villain = hero_villain
+        self.hero = hero
+        self.villain = villain
         self.hunter = hunter
         self.immobile = immobile
-        self.jedi_sith = jedi_sith
+        self.jedi = jedi
+        self.sith = sith
         self.jump = jump
         self.impact = impact
         self.impervious = impervious
@@ -223,7 +229,124 @@ class Model:
         self.slow = slow
         self.spotter = spotter
         self.take_cover = take_cover
+        self.unique = unique
         self.vehicle = vehicle
+
+        self.quality_cost_dict = {6: 2, 5: 4, 4: 6, 3: 8, 2: 12}
+        self.defense_cost_dict = {6: 2, 5: 4, 4: 6, 3: 8, 2: 12}
+
+        self.cover_cost_dict = {
+            None: 0,
+            "Front": 2,
+            "Left": 2,
+            "Right": 2,
+            "Rear": 2,
+            "Sides": 4,
+            "Front, Sides": 6,
+            "Rear, Sides": 6,
+        }
+        self.courage_cost_dict = {False: 0, True: 5}
+        self.command_cost_dict = {False: 0, True: 10}
+        self.deflect_cost_dict = {False: 0, True: 2}
+        self.droid_cost_dict = {False: 0, True: 0}
+        self.emplacement_cost_dict = {False: 0, True: 0}
+        self.expendable_cost_dict = {0: 0, 1: 0, 2: 0, 3: 0}
+        self.fast_cost_dict = {False: 0, True: 1}
+        self.fear_cost_dict = {False: 0, True: 5}
+        self.fly_cost_dict = {False: 0, True: 1}
+        self.hero_villain_cost_dict = {False: 0, True: 0}
+        self.hunter_cost_dict = {None: 0, "Jedi": 0.5, "Sith": 0.5}
+        self.immobile_cost_dict = {False: 0, True: -3}
+        self.jedi_sith_cost_dict = {False: 0, True: 0}
+        self.jump_cost_dict = {0: 0, 3: 0.5, 6: 1}
+        self.impervious_cost_dict = {False: 0, True: 6}
+        self.protector_cost_dict = {False: 0, True: 1}
+        self.relay_cost_dict = {False: 0, True: 5}
+        self.relentless_cost_dict = {False: 0, True: 1}
+        self.scout_cost_dict = {False: 0, True: 1}
+        self.slow_cost_dict = {False: 0, True: -1}
+        self.vehicle_cost_dict = {False: 0, True: 0}
+
+        if hero & villain:
+            raise Exception("Cannot select both Hero and Villain")
+        if jedi & sith:
+            raise Exception("Cannot select both Jedi and Sith")
+
+    def calculate_cost(self):
+
+        quality_cost = self.quality_cost_dict[self.quality]
+        defense_cost = self.defense_cost_dict[self.defense]
+
+        base_cost = (quality_cost + defense_cost) * self.toughness
+
+        cover_cost = self.cover_cost_dict[self.cover] * self.toughness
+        courage_cost = self.courage_cost_dict[self.courage]
+        command_cost = self.command_cost_dict[self.command]
+        deflect_cost = self.deflect_cost_dict[self.deflect] * self.toughness
+        droid_cost = self.droid_cost_dict[self.droid]
+        emplacement_cost = self.emplacement_cost_dict[self.emplacement]
+        expendable_cost = self.expendable_cost_dict[self.expendable]
+        fast_cost = self.fast_cost_dict[self.fast] * quality_cost
+        fear_cost = self.fear_cost_dict[self.fear]
+        fly_cost = self.fly_cost_dict[self.fly] * quality_cost
+        heal_cost = self.heal * 10
+        hero_cost = self.hero_villain_cost_dict[self.hero]
+        villain_cost = self.hero_villain_cost_dict[self.villain]
+        hunter_cost = self.hunter_cost_dict[self.hunter] * quality_cost
+        immobile_cost = self.immobile_cost_dict[self.immobile] * quality_cost
+        jedi_cost = self.jedi_sith_cost_dict[self.jedi]
+        sith_cost = self.jedi_sith_cost_dict[self.sith]
+        jump_cost = self.jump_cost_dict[self.jump] * quality_cost
+        impact_cost = self.impact * 3
+        impervious_cost = self.impervious_cost_dict[self.impervious] * self.toughness
+        protector_cost = self.protector_cost_dict[self.protector] * defense_cost
+        relay_cost = self.relay_cost_dict[self.relay]
+        relentless_cost = self.relentless_cost_dict[self.relentless] * quality_cost
+        repair_cost = 10 * self.repair
+        scout_cost = self.scout_cost_dict[self.scout] * quality_cost
+        shield_cost = self.shield * (
+            10 + quality_cost
+        )  # don't understand the logic here? Should it be defense?
+        slow_cost = self.slow_cost_dict[self.slow] * quality_cost
+        spotter_cost = self.spotter * 5
+        take_cover_cost = self.take_cover * 5
+        vehicle_cost = self.vehicle_cost_dict[self.vehicle]
+
+        model_cost = (
+            base_cost
+            + cover_cost
+            + courage_cost
+            + command_cost
+            + deflect_cost
+            + droid_cost
+            + emplacement_cost
+            + expendable_cost
+            + fast_cost
+            + fear_cost
+            + fly_cost
+            + heal_cost
+            + hero_cost
+            + villain_cost
+            + hunter_cost
+            + immobile_cost
+            + jedi_cost
+            + sith_cost
+            + jump_cost
+            + impact_cost
+            + impervious_cost
+            + protector_cost
+            + relay_cost
+            + relentless_cost
+            + repair_cost
+            + scout_cost
+            + shield_cost
+            + slow_cost
+            + spotter_cost
+            + take_cover_cost
+            + vehicle_cost
+        )
+
+        return model_cost
 
     def equip_weapon(self, weapon) -> None:
         pass
@@ -262,4 +385,61 @@ dioxis_grenade = Weapon(
 )
 print("%s, Qu 4+: %.2f pts" % (dioxis_grenade.name, dioxis_grenade.calculate_cost(4)))
 
-# %%
+# %% Base Model Test Cases
+
+stormtrooper = Model("Stormtrooper", 5, 4, 1, expendable=1)
+print("%s: %.2f pts" % (stormtrooper.name, stormtrooper.calculate_cost()))
+
+e_web = Model("E-Web Team", 4, 4, 3, emplacement=True, cover="Front", slow=True)
+print("%s: %.2f pts" % (e_web.name, e_web.calculate_cost()))
+
+id_seeker_droid = Model(
+    "ID Seeker Droid",
+    5,
+    5,
+    1,
+    droid=True,
+    shield=1,
+    protector=True,
+    scout=True,
+    fly=True,
+)
+print("%s: %.2f pts" % (id_seeker_droid.name, id_seeker_droid.calculate_cost()))
+
+darth_vader = Model(
+    "Darth Vader",
+    3,
+    3,
+    6,
+    sith=True,
+    fear=True,
+    deflect=True,
+    relentless=True,
+    impervious=True,
+    slow=True,
+    jump=3,
+    unique="Anakin Skywalker",
+)
+print("%s: %.2f pts" % (darth_vader.name, darth_vader.calculate_cost()))
+
+isf_commander = Model("ISF Commander", 3, 4, 3, villain=True, command=True, scout=True)
+print("%s: %.2f pts" % (isf_commander.name, isf_commander.calculate_cost()))
+
+stormtrooper_captain = Model("Stormtrooper Captain", 4, 4, 3, villain=True, relay=True)
+print(
+    "%s: %.2f pts" % (stormtrooper_captain.name, stormtrooper_captain.calculate_cost())
+)
+
+imperial_officer = Model("Imperial Officer", 5, 6, 1, take_cover=1)
+print("%s: %.2f pts" % (imperial_officer.name, imperial_officer.calculate_cost()))
+
+purge_trooper = Model("Purge Trooper", 4, 4, 1, hunter="Jedi", impervious=True)
+print("%s: %.2f pts" % (purge_trooper.name, purge_trooper.calculate_cost()))
+
+medical_droid = Model("Medical Droid", 6, 5, 1, droid=True, heal=1, slow=True)
+print("%s: %.2f pts" % (medical_droid.name, medical_droid.calculate_cost()))
+
+astromech_droid = Model("Astromech Droid", 5, 5, 1, droid=True, repair=1, slow=True)
+print("%s: %.2f pts" % (astromech_droid.name, astromech_droid.calculate_cost()))
+
+# continue checking costs; go through CIS units
