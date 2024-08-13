a6076fb5066dc894fb56c1b3a2ef4fe4c14bcdd8
Rupert Eardley <rupert.eardley@gmail.com>
2024-08-13 08:02:52 +0100

    Update syndicates (add black sun, rebalance existing profiles)

--- faction_rosters/syndicates/crime_syndicates.py
+++ faction_rosters/syndicates/crime_syndicates.py
@@ -12,14 +12,18 @@ tsv_file = "crime_syndicates.tsv"
 """ Crimson Dawn """
 
 # common weapons
-percussive_cannon = Weapon("Percussive Cannon", 18, 1, pierce=2, deadly=3, ammo=2)
+percussive_cannon = Weapon(
+    "Percussive Cannon", 15, 1, pierce=2, deadly=3, ammo=2, sniper=True, suppressive=1
+)
 
 # models
-hylobon_captain = Model("Hylobon Captain", 3, 5, 3, relay=True, villain=True, spotter=2)
+hylobon_captain = Model(
+    "Hylobon Captain", 4, 5, 3, relay=True, villain=True, spotter=2, disciplined=True
+)
 hylobon_captain.equip_weapon(core.combat_training)
 hylobon_captain.equip_weapon(percussive_cannon)
 
-hylobon_enforcer = Model("Hylobon Enforcer", 4, 5, 1)
+hylobon_enforcer = Model("Hylobon Enforcer", 5, 5, 1, disciplined=True)
 hylobon_enforcer.equip_weapon(core.combat_training)
 hylobon_enforcer.equip_weapon(percussive_cannon)
 
@@ -39,11 +43,10 @@ list_crimson_dawn.file_write_tsv(tsv_file, list_title="Crimson Dawn")
 
 """ Pyke Syndicate """
 
-pyke_capo = Model("Pyke Capo", 3, 5, 3, command=True, take_cover=1)
+pyke_capo = Model("Pyke Capo", 4, 5, 3, command=True, take_cover=1, villain=True)
 pyke_capo.equip_weapon(core.blaster_pistol)
 
-pyke_soldier = Model("Pyke Soldier", 4, 5, 1, expendable=1)
-# pyke_soldier.equip_weapon(core.truncheon)
+pyke_soldier = Model("Pyke Soldier", 5, 5, 1, expendable=1)
 pyke_soldier.equip_weapon(core.blaster_pistol)
 
 # -*- Upgrade lists -*-
@@ -58,7 +61,8 @@ upgrade_pyke_add = UpgradeList(label, base_model=pyke_soldier)
 upgrade_pyke_add.select_upgrade_with_weapon_type(limit=1, lose_expendable=True)
 upgrade_pyke_add.upgrade_with_weapon_entry(core.sniper_rifle)
 upgrade_pyke_add.upgrade_with_weapon_entry(core.heavy_sniper_rifle)
-upgrade_pyke_add.upgrade_with_weapon_entry(core.truncheon, manual_points_adjustment=-4)
+upgrade_pyke_add.upgrade_with_weapon_entry(core.truncheon)
+upgrade_pyke_add.upgrade_with_weapon_entry(core.stun_spear)
 upgrade_pyke_add.upgrade_with_weapon_entry(core.electrowhip)
 upgrade_pyke_add.upgrade_with_weapon_entry(core.thermal_detonator)
 upgrade_pyke_add.upgrade_with_weapon_entry(core.concussion_grenade)
@@ -90,8 +94,70 @@ list_pykes.file_write_tsv(tsv_file, list_title="Pyke Syndicate", append=True)
 upgrade_pyke_replace.file_write_tsv(tsv_file)
 upgrade_pyke_add.file_write_tsv(tsv_file)
 
-
 """ Black Sun """
+
+# common weapons
+double_blaster = Weapon("Double Blaster", 9, 4, inaccurate=True)
+mag_det = Weapon(
+    "Mag-Det Charge",
+    6,
+    1,
+    pierce=3,
+    blast=3,
+    deadly=2,
+    indirect=True,
+    seek=True,
+    ammo="Single Use",
+)
+
+# models
+
+black_sun_vigo = Model("Black Sun Vigo", 4, 4, 3, command=True, spotter=2, villain=True)
+black_sun_vigo.equip_weapon(core.combat_training)
+black_sun_vigo.equip_weapon(double_blaster)
+
+black_sun_captain = Model(
+    "Black Sun Captain", 4, 4, 3, relay=True, hunter="Target", villain=True
+)
+black_sun_captain.equip_weapon(core.combat_training)
+black_sun_captain.equip_weapon(double_blaster)
+
+black_sun_soldier = Model("Black Sun Enforcer", 4, 4, 1, expendable=1)
+black_sun_soldier.equip_weapon(core.combat_training)
+black_sun_soldier.equip_weapon(double_blaster)
+
+# -*- Upgrade lists -*-
+
+label = letter_increment(label)
+upgrade_black_sun = UpgradeList(label, base_model=black_sun_soldier)
+upgrade_black_sun.select_upgrade_with_weapon_type(limit=2, lose_expendable=True)
+upgrade_black_sun.upgrade_with_weapon_entry(core.scattergun)
+upgrade_black_sun.upgrade_with_weapon_entry(core.thermal_detonator)
+upgrade_black_sun.upgrade_with_weapon_entry(core.concussion_grenade)
+upgrade_black_sun.upgrade_with_weapon_entry(mag_det)
+upgrade_black_sun.upgrade_with_weapon_entry(core.frag_grenade)
+upgrade_black_sun.upgrade_with_weapon_entry(core.thermal_imploder)
+
+# assign upgrade lists
+
+black_sun_soldier.add_upgrade_list(upgrade_black_sun)
+
+# collate model list
+
+list_black_sun = ModelList()
+list_black_sun.add_model_entry(black_sun_vigo)
+list_black_sun.add_model_entry(black_sun_captain)
+list_black_sun.add_model_entry(black_sun_soldier)
+
+# write latex files
+
+list_black_sun.file_write_latex("syndicates_black_sun_roster.tabl")
+upgrade_black_sun.file_write_latex()
+
+# write tsv files
+
+list_black_sun.file_write_tsv(tsv_file, list_title="Black Sun", append=True)
+upgrade_black_sun.file_write_tsv(tsv_file)
 
 """ Crime Lords """
 
